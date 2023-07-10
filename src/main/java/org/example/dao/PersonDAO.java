26e5b89b707960e5e5feafa84234c4e80527ceff
ÿagodko-ana <anastasia.yagodko@gmail.com>
2023-07-10 19:25:04 +0300

    READ: created DAO, methods, pages

---
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package org.example.dao;
+
+import org.example.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static  int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Jerry"));
+        people.add(new Person(++PEOPLE_COUNT, "Vasya"));
+        people.add(new Person(++PEOPLE_COUNT, "Andrew"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show (int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
