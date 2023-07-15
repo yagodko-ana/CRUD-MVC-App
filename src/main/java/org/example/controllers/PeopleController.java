0be80bcfc829c79a90f412842142ea999abd5ffe
ÿagodko-ana <anastasia.yagodko@gmail.com>
2023-07-15 18:47:33 +0300

    VALID: added fields' validation using hibernate validator

--- src/main/java/org/example/controllers/PeopleController.java
+++ src/main/java/org/example/controllers/PeopleController.java
@@ -5,7 +5,10 @@ import org.example.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -48,9 +51,12 @@ public class PeopleController {
     }
 
     @PostMapping
-    public String create(@ModelAttribute("person") Person person) {
-        personDAO.save(person);
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
 
+        personDAO.save(person);
         return "redirect:/people";
     }
 
@@ -61,7 +67,11 @@ public class PeopleController {
     }
 
     @PostMapping ("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
